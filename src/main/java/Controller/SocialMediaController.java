4cec2e557bdde4a394cd6d5aaf5cddbba80bb389
Marcell Fulop <marcellfulop2000@gmail.com>
2024-04-16 10:23:30 +0000

    wrote one controller method

--- src/main/java/Controller/SocialMediaController.java
+++ src/main/java/Controller/SocialMediaController.java
@@ -3,12 +3,22 @@ package Controller;
 import io.javalin.Javalin;
 import io.javalin.http.Context;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import Model.Account;
+import Service.*;
+
 /**
  * TODO: You will need to write your own endpoints and handlers for your controller. The endpoints you will need can be
  * found in readme.md as well as the test cases. You should
  * refer to prior mini-project labs and lecture materials for guidance on how a controller may be built.
  */
 public class SocialMediaController {
+    private SocialMediaService service;
+    public SocialMediaController(){
+        this.service = new SocialMediaService();
+    }
     /**
      * In order for the test cases to work, you will need to write the endpoints in the startAPI() method, as the test
      * suite must receive a Javalin object from this method.
@@ -16,11 +26,23 @@ public class SocialMediaController {
      */
     public Javalin startAPI() {
         Javalin app = Javalin.create();
+        app.post("/register", this::createUser);
         app.get("example-endpoint", this::exampleHandler);
 
         return app;
     }
 
+    private void createUser(Context ctx) throws JsonProcessingException {
+        ObjectMapper mapper = new ObjectMapper();
+        Account newUser = mapper.readValue(ctx.body(), Account.class);
+        Account addedUser = service.createAccount(newUser);
+        if (addedUser == null){
+            ctx.status(400);
+        }else{
+            ctx.status(200);
+            ctx.json(mapper.writeValueAsString(addedUser));
+        }
+    }
     /**
      * This is an example handler for an example endpoint.
      * @param context The Javalin Context object manages information about both the HTTP request and response.
